5f03784446b6ce4450c24a1a77c7bb1121caa554
jeniferlaisa <jeniferlaisa@hotmail.com>
2020-11-19 20:30:23 -0300

    Unit tests and API Test cases

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from .models import Seller,Product
+from rest_framework.test import APIClient
+import json
 
-# Create your tests here.
+# Unit tests
+class SellerModelTest(TestCase):
+    def test_class_str(self):
+        seller = Seller()
+        seller.name = "Samsung"
+        self.assertEquals(seller.__str__(), "Samsung")
+
+class ProductModelTest(TestCase):
+    def test_class_str(self):
+        product = Product()
+        seller = Seller()
+        seller.name = "Samsung"
+        product.title = "Rádio FM"
+        product.price = 499.99
+        product.code = "aaf4a3r3"
+        product.seller = seller
+        product.stock_quantity = 60 
+        product.product_status = "A"
+        self.assertEquals(product.__str__(), "Rádio FM - aaf4a3r3")
+         
+# API Test cases
+class SellerViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Seller.objects.create(name="LG")
+
+    def test_get(self):
+        client = APIClient()
+        response = client.get('/products/sellers/')
+
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+
+        self.assertEqual(data.get('count'), 1)
+
+        sellers1 = data.get('results')[0]
+
+        self.assertEqual(sellers1.get("name"), "LG")
+      
+    def test_post(self):
+        client = APIClient()
+        response = client.post('/products/sellers/', {
+            "name": "Whirpool",
+        })
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEquals(Seller.objects.count(), 2)
+        self.assertEquals(Seller.objects.last().name, "Whirpool")
